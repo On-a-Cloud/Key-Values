6de41c87cdaacacf6a9a67cdaaff634d7554c871
Humberto Banuelos <humberto.banuelos13@outlook.com>
2018-10-21 19:53:55 -0400

    Add project files.

---
+++ Key-Values/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new MyDictionary();
+            try
+            {
+                Console.WriteLine(d["Cats"]);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            d["Cats"] = 42;
+            d["Dogs"] = 17;
+            Console.WriteLine($"{(int)d["Cats"]}, {(int)d["Dogs"]}");
+        }
+    }
+}
